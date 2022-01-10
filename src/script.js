66f3964fc96df7a35751e59a0912b4451c13cba7
CrawLeyYou <kaanbenkansoy@gmail.com>
2022-01-10 18:46:15 +0300

    Update After 2 Months
    asdasdas

--- src/script.js
+++ src/script.js
@@ -9,6 +9,18 @@ var avatar = config.options.avatar //Get avatar url
 var content = config.options.content //Get content value
 var username = config.options.username //Get username
 var maxmessage = config.options.maxmessage //Get maxmessage value
+var embed = config.embed.status //Get embed status
+var ping = config.embed.ping_only //Get ping status
+var embedtitle = config.embed.title //Get embed title
+var embedtitleurl = config.embed.title_url //Get embed title url
+var embeddescription = config.embed.description //Get embed description
+var embedcolor = config.embed.color //Get embed color
+var embedfootertext = config.embed.footer.text //Get embed footer text
+var embedfootericon = config.embed.footer.icon_url //Get embed footer icon url
+var embedimage = config.embed.image.url //Get embed image url
+var embedauthor = config.embed.author.name //Get embed author name
+var embedauthorurl = config.embed.author.url //Get embed author url
+var embedauthoricon = config.embed.author.icon_url //Get embed author icon url
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -23,6 +35,43 @@ async function WebhookSender(content) {
             username: username,
             content: content,
             avatar_url: avatar
+        })
+        .then(function (response) {
+            console.log("Webhook Succesfully Sended!")
+            //Doesnt logs the response cuz we dont need that.
+        })
+        .catch(function (error) {
+            console.log("A error occurred while sending webhook.");
+        });
+}
+
+async function EmbedWebhookSender(content) {
+    await axios.post(webhook, {
+            username: username,
+            content: content,
+            avatar_url: avatar,
+
+            embeds: [{
+            title: embedtitle,
+            url: embedtitleurl,
+            description: embeddescription,
+            color: embedcolor,
+            footer: {
+                text: embedfootertext,
+                icon_url: embedfootericon
+            },
+
+            image: {
+                url: embedimage
+            },
+
+            author: {
+                name: embedauthor,
+                url: embedauthorurl,
+                icon_url: embedauthoricon
+            },
+        }
+        ]
         })
         .then(function (response) {
             console.log("Webhook Succesfully Sended!")
@@ -48,7 +97,15 @@ async function Smart(n1, n2) {
     if (n2 != maxmessage) {
         if (n1 != 30) {
             for (var i = 0; i < 5; i++) {
+                if (embed != true) {
                 await WebhookSender(content)
+                } else {
+                    if (ping == true) {
+                        await EmbedWebhookSender("@everyone")
+                    } else {
+                        await EmbedWebhookSender(content)
+                    }
+                }
             }
             sleep(2000)
             Smart(n1 + 5, n2)
